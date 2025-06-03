b14b749c42f57b9686bc33482b7bf8f651013749
Rances Cuizon <rancescuizon@gmail.com>
2025-06-03 21:48:40 +0800

    Add: home-companion card, list and cta

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
+import { CompanionCard } from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my App</h1>
-      <Button>Let's get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard 
+          id="123"
+          name="Companion One"
+          topic="This is a description of Companion One."
+          subject="science"
+          duration={45}
+          color="ffda6ae"
+        />
+        <CompanionCard 
+          id="456"
+          name="Companion One"
+          topic="This is a description of Companion Two."
+          subject="science"
+          duration={45}
+          color="blue"
+        />
+        <CompanionCard 
+          id="789"
+          name="Companion One"
+          topic="This is a description of Companion Three."
+          subject="science"
+          duration={45}
+          color="red"
+        />
+        
+      </section>
+
+      <section className="home-section">
+        <CompanionsList 
+          title= "Recent Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
