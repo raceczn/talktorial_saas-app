333137f0337e3bbda25c6d9c09e8848095413938
Rances Cuizon <rancescuizon@gmail.com>
2025-06-03 23:37:02 +0800

    feat: integrate Clerk for authentication, billing for plans/subscription and add CompanionForm with validation (shadcn)

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -2,7 +2,6 @@ const CompanionsLibrary = () => {
   return (
     <div>
       <h1 className="text-2xl underline">Companions</h1>
-      <p className="mt-4">This page will list all your companions.</p>
     </div>
   );
 }
