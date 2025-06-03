017627dcc42954012cec782a7a9cc78b48220e4b
Rances Cuizon <rancescuizon@gmail.com>
2025-06-03 18:55:24 +0800

    first commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,9 @@
+const CompanionsLibrary = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">Companions</h1>
+      <p className="mt-4">This page will list all your companions.</p>
+    </div>
+  );
+}
+export default CompanionsLibrary;
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Page = () => { 
+  return (
+    <div>
+      <h1 className="text-2xl underline">Sign In</h1>
+
+    </div>
+  );
+}
+
+export default Page;
