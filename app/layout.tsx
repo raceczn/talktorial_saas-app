a185afe6599f1c52d6a32ca352c4d0afdeee9eed
Rances Cuizon <rancescuizon@gmail.com>
2025-06-04 17:33:38 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+
+export const dynamic = "force-dynamic";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
